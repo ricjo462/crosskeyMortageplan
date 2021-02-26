174b54dfd098b71acdc41249b62301918af57f24
richardjohansson <richardjohansson1999@gmail.com>
2021-02-26 15:03:47 +0100

    Add customer class to keep tack of all customer data

---
+++ src/main/java/Customer.java
@@ -0,0 +1,34 @@
+public class Customer {
+    String name;
+    double totalLoan;
+    double interest;
+    double years;
+    double monthlyPayment;
+
+    public Customer(String name, double totalLoan, double interest, double years) {
+        this.name = name;
+        this.totalLoan = totalLoan;
+        this.interest = interest;
+        this.years = years;
+    }
+
+    public void setMonthlyPayment(double monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public double getTotalLoan() {
+        return this.totalLoan;
+    }
+
+    public double getInterest() {
+        return this.interest;
+    }
+
+    public double getYears() {
+        return this.years;
+    }
+
+    public double getMonthlyPayment() {
+        return this.monthlyPayment;
+    }
+}
