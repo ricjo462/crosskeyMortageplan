efd9dfb04dc3e47a285e4d585605711078286fb0
richardjohansson <richardjohansson1999@gmail.com>
2021-03-01 11:16:46 +0100

    Add test for Reader

---
+++ src/test/java/ReaderTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReaderTest {
+    Reader reader = new Reader();
+    PrintStream standardOut = System.out;
+    ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void testReadNoFile() {
+        reader.read("");
+        assertEquals("No prospects file found named:", outputStreamCaptor.toString().trim());
+    }
+
+    @After
+    public void reset() {
+        System.setOut(standardOut);
+    }
+}
